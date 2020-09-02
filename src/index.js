c1ae8e4518da7ee345d3cbe1fc133d7e61d6c050
naydyonov <naydyonov@etm-system.ru>
2020-09-02 18:48:03 +0300

    cool materials

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
+import {model} from './module'
+import {Site} from './classes/site'
+import {Sidebar} from './classes/sidebar'
 import './styles/main.css'
+
+const site = new Site('#site')
+
+const updateCallback = (newBlock) => {
+    model.push(newBlock)
+    site.render(model)
+}
+
+new Sidebar('#panel', updateCallback)
+
+site.render(model)
+
 
--- src/module.js
+++ src/module.js
@@ -1,1 +1,29 @@
-console.log('i am module.js')
+import image from './assets/image.png'
+import {TitleBlock, ImageBlock, TextBlock, TextColumnsBlock} from './classes/blocks'
+import {css} from "./utils";
+
+export const model = [
+  new TitleBlock('Конструктор сайтов на чистом JavaScript!',{
+    tag: 'h2',
+    styles: css({
+      background: 'darkred',
+      color: "#fff",
+      'text-align': 'center'
+    })
+  }),
+  new ImageBlock(image, {
+    styles: 'padding: 2rem 0; display: flex; justify-content: center',
+    alt: 'image',
+    imageStyles: 'with: 500px; height: auto'
+  }),
+  new TextColumnsBlock([
+    'Приложение на чистом JavaScript без использования библиотек',
+    'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс ',
+    'JavaScript - это просто, интересно. Научись создавать UI своими руками'
+  ], {
+    styles: 'padding: 1rem'
+  }),
+  new TextBlock('Lorem ipsum dolor sit amet, consectetur adipisicing elit.', {
+    styles: 'background: darkblue;color:yellow'
+  })
+]
